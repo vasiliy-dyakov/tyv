792a0cdb7bd45d10cd55a739d7c9fcf270c752df
Vasilii Diakov <vasilij.dyakov@gmail.com>
2014-11-02 00:22:45 +0300

    Partials на js templates

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,11 +1,28 @@
 'use strict';
 
-var TYV = angular.module('TYV', []);
+var TYV = angular.module('TYV', ['ngRoute']);
 
-TYV.controller('StepsController', function($scope) {
+TYV.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/step/:stepNum', {
+            templateUrl: 'steps',
+            controller: 'StepsController'
+        })
+        .otherwise({
+            redirectTo: '/step/1'
+        });
+    }
+]);
+
+TYV.controller('StepsController', ['$scope', '$routeParams', function($scope, $routeParams) {
+
+    $scope.stepsCount = 3;
+    $scope.step = +$routeParams.stepNum;
+    $scope.nextStep = $scope.step < $scope.stepsCount ? $scope.step + 1 : false;
+
     $scope.words = ['вода', 'рис', 'музыка', 'спорт', 'задача',
         'собака', 'покер', 'планировщик', 'победа', 'автомобиль',
         'жечь', 'летать', 'мыть', 'слыть', 'шашка',
         'такса', 'маржа', 'филигранный', 'зомби', 'карусель'
     ];
-});
+}]);
