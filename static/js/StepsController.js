804fda7ce90681ff332bd46403aeca2edaefb5c4
Vasilii Diakov <vasilij.dyakov@gmail.com>
2014-11-09 19:02:23 +0300

    Округление результатов и правки css

--- static/js/StepsController.js
+++ static/js/StepsController.js
@@ -25,7 +25,7 @@
         $scope.step = +$routeParams.stepNum;
         $scope.nextStep = $scope.step < $scope.stepsCount ? $scope.step + 1 : false;
 
-        if ($scope.step > 1 && typeof storage.get('currentResult') === 'undefined') {
+        if ($scope.step > 1 && typeof storage.get('currentResultRounded') === 'undefined') {
             this.redirect('/');
         }
 
@@ -39,7 +39,7 @@
             });
         } else {
             $http.get('/data/words2.json').success(function(data) {
-                $scope.words = data[storage.get('currentResult')].map(function(word) {
+                $scope.words = data[storage.get('currentResultRounded')].map(function(word) {
                     return _.extend({
                             checked: false
                         }, word);
@@ -61,6 +61,7 @@
                         checked: true
                     }).length,
                 currentResult,
+                currentResultRounded,
                 knownWords;
 
             isCheating && this.storage.set('isCheating', isCheating);
@@ -68,14 +69,16 @@
             if (this.$scope.step === 1) {
 
                 currentResult = Math.floor(48000 * checkedCount / this.$scope.words.length);
-                // округляем до нижних 10000
-                currentResult = Math.round(Math.floor(currentResult / 10000) * 10000);
                 this.storage.set('currentResult', currentResult);
+                // округляем до нижних 10000
+                currentResultRounded = Math.round(Math.floor(currentResult / 10000) * 10000);
+                this.storage.set('currentResultRounded', currentResultRounded);
 
             } else {
 
-                knownWords = this.storage.get('currentResult') +
-                    Math.round((this.storage.get('currentResult') < 40000 ? 10000 : 8000) * checkedCount / this.$scope.words.length);
+                knownWords = this.storage.get('currentResultRounded') +
+                    Math.round((this.storage.get('currentResultRounded') < 40000 ? 10000 : 8000) * checkedCount / this.$scope.words.length);
+                knownWords = Math.round((this.storage.get('currentResult') + knownWords) / 2);
                 this.storage.set('knownWords', knownWords);
 
             }
