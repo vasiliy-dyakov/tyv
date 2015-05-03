58f2fb03ec3e219cf93aa8dbf977428ef0de3e8a
Vasilii Diakov <vasilij.dyakov@gmail.com>
2015-05-03 16:23:31 +0300

    Переименовал файл unit-теста

--- test/common.js
+++ test/common.js
@@ -5,7 +5,7 @@ require.config({
 require(
     Object.keys(window.__karma__.files)
         .filter(function(name) {
-            return /(spec|test)\.js$/i.test(name);
+            return /\.test\.js$/i.test(name);
         })
         .map(function(name) {
             return name.replace(/^\/base\//, '../../').replace(/\.js$/, '');
